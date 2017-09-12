006f406e2461187e0ad9f95479eef2913a8a80a8
Dima Vakhnenko <qukobox@gmail.com>
2017-09-13 00:27:30 +0300

    Use es6, Luke

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export default function wrap(classes, prefix) {
-  var value
-  var className = ""
-  var type = typeof classes
+  let value
+  let className = ""
+  let type = typeof classes
 
   if ((classes && type === "string") || type === "number") {
     return classes
@@ -10,13 +10,13 @@ export default function wrap(classes, prefix) {
   prefix = prefix || " "
 
   if (Array.isArray(classes) && classes.length) {
-    for (var i = 0; i < classes.length; i++) {
+    for (let i = 0; i < classes.length; i++) {
       if ((value = wrap(classes[i], prefix))) {
         className += (className && prefix) + value
       }
     }
   } else {
-    for (var i in classes) {
+    for (let i in classes) {
       if ((value = classes[i])) {
         className +=
           (className && prefix) +
