2be2ec8488135646c572907b20511a8686d53f9e
Swizz <gerodel.quentin@gmail.com>
2017-09-14 21:43:33 +0200

    Add JSDOC

--- classwrap.d.ts
+++ classwrap.d.ts
@@ -1,4 +1,3 @@
-
 export as namespace classwrap
 
 export interface ClassSet {
@@ -7,4 +6,12 @@ export interface ClassSet {
 
 export type Class = string | number | ClassSet
 
+/**
+ * Joins all elements of an array or keys of an object into a string.
+ * If the value associated with a given key is falsy, the key will be ignored.
+ * Nested arrays or objects are supported too. Use this feature to assemble
+ * classes with a common prefix.
+ *
+ * @param classes
+*/
 export default function (classes: Class | Class[])
