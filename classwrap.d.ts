60a92a320b31d8c5b58db71e9298dafce05a8a87
Swizz <gerodel.quentin@gmail.com>
2017-09-14 21:39:40 +0200

    Add ClasSet interface

--- classwrap.d.ts
+++ classwrap.d.ts
@@ -1,6 +1,10 @@
 
 export as namespace classwrap
 
-export type Class = string | number
+export interface ClassSet {
+  [key: string]: boolean | any | ClassSet
+}
+
+export type Class = string | number | ClassSet
 
 export default function (classes: Class | Class[])
