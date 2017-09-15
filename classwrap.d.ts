86997650a96f86ac5bd6dc93e4393b8221e8be11
Swizz <gerodel.quentin@gmail.com>
2017-09-15 12:22:22 +0200

    Simpler JSDoc

--- classwrap.d.ts
+++ classwrap.d.ts
@@ -8,9 +8,6 @@ export type Class = string | number | ClassSet
 
 /**
  * Joins all elements of an array or keys of an object into a string.
- * If the value associated with a given key is falsy, the key will be ignored.
- * Nested arrays or objects are supported too. Use this feature to assemble
- * classes with a common prefix.
  *
  * @param classes
 */
