76852c6a23f97403b06b76b027526e721145cf29
Alexandre Nicolas <Kornflexx@users.noreply.github.com>
2018-05-14 17:12:21 +0200

    Improve performance
    For ... in is very slow.
    
    _Add Object.keys to retrieve keys.
    _Use for (let i ...) : faster iterator existing for now (in some case forEach appears to be faster but it's pretty uncommon).
    _Use let & const instead of var.
    
    I didn't run a benchmark but I'm sure it will be faster.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 export default function cc(classes) {
-  var out = ""
-  var type = typeof classes
+  let out = ""
+  const type = typeof classes
 
   if (type === "string" || type === "number") {
     return classes || ""
   }
 
   if (Array.isArray(classes) && classes.length > 0) {
-    for (var i = 0, len = classes.length; i < len; i++) {
-      var next = cc(classes[i])
+    for (let i = 0, len = classes.length; i < len; i++) {
+      const next = cc(classes[i])
       if (next) {
         out += (out && " ") + next
       }
     }
-  } else {
-    for (var key in classes) {
-      if (classes.hasOwnProperty(key) && classes[key]) {
-        out += (out && " ") + key
-      }
+    return out
+  }
+  
+  const keys = Object.keys(classes)
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    if (classes[key]) {
+      out += (out && " ") + key
     }
   }
 
