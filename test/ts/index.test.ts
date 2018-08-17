4c99f12001a1da16cac5b753cb4c640ad448ee22
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-18 01:28:02 +0900

    test: pretty ts test file

--- test/ts/index.test.ts
+++ test/ts/index.test.ts
@@ -3,19 +3,8 @@ import cc from "classcat"
 let className: string
 
 className = cc(0)
-
 className = cc("btn")
-
 className = cc(["btn", "btn-active"])
-
 className = cc([["btn", "btn-active"]])
-
-className = cc({
-  btn: true
-})
-
-className = cc([
-  {
-    btn: true
-  }
-])
+className = cc({ btn: true })
+className = cc([{ btn: true }])
