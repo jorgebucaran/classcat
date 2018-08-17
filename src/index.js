0bd53dbef6228651677073225b50fd4aba395026
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-18 02:04:24 +0900

    perf: cache isArray; declare vars on top

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-export default function cc(classes) {
+var isArray = Array.isArray
+
+export default function cc(cls) {
+  var i
+  var len
+  var key
   var out = ""
-  var type = typeof classes
+  var type = typeof cls
+  var next
 
-  if (type === "string" || type === "number") {
-    return classes || ""
-  }
+  if (type === "string" || type === "number") return cls || ""
 
-  if (Array.isArray(classes) && classes.length > 0) {
-    for (var i = 0, len = classes.length; i < len; i++) {
-      var next = cc(classes[i])
-      if (next) {
+  if (isArray(cls) && cls.length > 0) {
+    for (i = 0, len = cls.length; i < len; i++) {
+      if ((next = cc(cls[i]))) {
         out += (out && " ") + next
       }
     }
   } else {
-    for (var key in classes) {
-      if (classes.hasOwnProperty(key) && classes[key]) {
+    for (key in cls) {
+      if (cls.hasOwnProperty(key) && cls[key]) {
         out += (out && " ") + key
       }
     }
