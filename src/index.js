1e516b0697e4f8e5cf3a8e9288ef679e94cdc97b
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-18 13:36:33 +0900

    refactor: avoid boolean coercion after recursive cc call

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 var isArray = Array.isArray
 
-export default function cc(cls) {
+export default function cc(names) {
   var i,
     len,
-    key,
     out = "",
-    type = typeof cls,
+    type = typeof names,
     next
 
-  if (type === "string" || type === "number") return cls || ""
+  if (type === "string" || type === "number") return names || ""
 
-  if (isArray(cls) && cls.length > 0) {
-    for (i = 0, len = cls.length; i < len; i++) {
-      if ((next = cc(cls[i]))) {
-        out += (out && " ") + next
-      }
+  if (isArray(names) && names.length > 0) {
+    for (i = 0, len = names.length; i < len; i++) {
+      if ((next = cc(names[i])) !== "") out += (out && " ") + next
     }
   } else {
-    for (key in cls) {
-      if (cls.hasOwnProperty(key) && cls[key]) {
-        out += (out && " ") + key
-      }
+    for (i in names) {
+      if (names.hasOwnProperty(i) && names[i]) out += (out && " ") + i
     }
   }
 
