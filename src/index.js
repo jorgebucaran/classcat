260768d152b26431b668244e34fd0e4a35abee41
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-18 14:14:25 +0900

    refactor: rename next→tmp

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ var isArray = Array.isArray
 export default function cc(names) {
   var i,
     len,
+    tmp,
     out = "",
-    type = typeof names,
-    next
+    type = typeof names
 
   if (type === "string" || type === "number") return names || ""
 
   if (isArray(names) && names.length > 0) {
     for (i = 0, len = names.length; i < len; i++) {
-      if ((next = cc(names[i])) !== "") out += (out && " ") + next
+      if ((tmp = cc(names[i])) !== "") out += (out && " ") + tmp
     }
   } else {
     for (i in names) {
