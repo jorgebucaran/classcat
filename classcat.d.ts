a93f022ead562a7ce9608a2fadc861aa43c00b45
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-18 14:13:42 +0900

    docs: revise library info in .d.ts file

--- classcat.d.ts
+++ classcat.d.ts
@@ -9,10 +9,10 @@ export interface ClassArray extends Array<Class> {}
 export type Class = string | number | ClassObject | ClassArray
 
 /**
- * Classcat is a unary function (accepts a single argument) expecting an array of elements or an object of keys and returns a string that is the result of joining all elements of the array or object keys.
+ * Classcat is a unary function expecting an array of elements _or_ an object of key/value pairs and returns a string that is the result of joining all the elements in the array or object keys.
  *
- * If the value associated with a given key is falsey, the key will be ignored.
+ * [Falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy) array elements and object properties will be ignored.
  *
- * @param classes
+ * @param classNames
  */
-export default function(classes: Class): string
+export default function(classNames: Class): string
