91e669ef9b9b6f8147f914fb6befc10e6f5451f9
Nuno Ribeiro <nuno@unitelabs.ch>
2018-12-03 17:10:11 +0100

    Added string array to allow a string array to be passed to the classcat() function. Tested

--- classcat.d.ts
+++ classcat.d.ts
@@ -6,7 +6,7 @@ export interface ClassObject {
 
 export interface ClassArray extends Array<Class> {}
 
-export type Class = string | number | ClassObject | ClassArray
+export type Class = string[] | string | number | ClassObject | ClassArray
 
 /**
  * A number, string, object or array. Objects consist of className/value pairs. Arrays are recursively reduced, therefore elements can be of any type aforementioned. [Truthy](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) values are added to the output, [falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy) values are ignored.
