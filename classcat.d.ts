35ee9194c8de6a5177b5ff2b05c627d2dab8d7bb
Jorge Bucaran <mail@jorgebucaran.com>
2019-06-28 03:41:48 +0900

    Remove confusing description

--- classcat.d.ts
+++ classcat.d.ts
@@ -8,9 +8,4 @@ export interface ClassArray extends Array<Class> {}
 
 export type Class = string | number | ClassObject | ClassArray
 
-/**
- * A number, string, object or array. Objects consist of className/value pairs. Arrays are recursively reduced, therefore elements can be of any type aforementioned. [Truthy](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) values are added to the output, [falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy) values are ignored.
- *
- * @param names
- */
 export default function(names: Class): string
