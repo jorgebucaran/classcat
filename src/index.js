e440cf922e0d8fc2e13f65c8258bd17f20f8fda4
Jorge Bucaran <mail@jorgebucaran.com>
2019-06-29 03:19:43 +0900

    Cut a few bytes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 var isArray = Array.isArray
 
 export default function cc(names) {
-  var i,
-    len,
-    tmp,
-    out = "",
-    type = typeof names
+  var i
+  var len
+  var tmp = typeof names
+  var out = ""
 
-  if (type === "string" || type === "number") return names || ""
+  if (tmp === "string" || tmp === "number") return names || ""
 
   if (isArray(names) && names.length > 0) {
     for (i = 0, len = names.length; i < len; i++) {
