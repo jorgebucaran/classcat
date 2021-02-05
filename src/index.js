a3fb1564804cc57ef10c56809fd4a7cf10c1190c
Jorge Bucaran <mail@jorgebucaran.com>
2021-02-05 15:44:30 +0900

    Use let and const

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-var isArray = Array.isArray
+const isArray = Array.isArray
 
 export default function cc(obj) {
   if (typeof obj === "string" || typeof obj === "number") return "" + obj
 
-  var out = ""
+  let out = ""
   if (isArray(obj))
-    for (var i = 0, tmp; i < obj.length; i++) {
+    for (let i = 0, tmp; i < obj.length; i++) {
       if ((tmp = cc(obj[i])) !== "") {
         out += (out && " ") + tmp
       }
     }
   else
-    for (var k in obj) {
+    for (let k in obj) {
       if (obj.hasOwnProperty(k) && obj[k]) out += (out && " ") + k
     }
 
