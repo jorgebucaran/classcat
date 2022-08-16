e69ed1b77485e38dcdb4e8afd2489933d94339b7
The web walker <contact@motss.app>
2022-08-16 21:07:19 +0800

    fix: add missing supported types in index.d.ts
    Add missing `null` and `undefined` in the typings. No code change is needed as the function already supports `null` and `undefined`.

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,7 @@ export interface ClassObject {
 
 export interface ClassArray extends Array<Class> {}
 
-export type Class = string | number | ClassObject | ClassArray
+export type Class = string | number | null | undefined | ClassObject | ClassArray
 
 /**
  * @param names A string, array or object of CSS class names (as keys).
